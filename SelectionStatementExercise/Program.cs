69bf57700eba458e69cef1e950d96d03573528df
Sionylen <sionylenpierce@outlook.com>
2022-11-28 15:44:19 -0600

    submit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Linq.Expressions;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is my favorite number?");
+            int userGuess = int.Parse(Console.ReadLine());
+            var myFavoriteNum = 8;
+
+            if (userGuess < 8)
+            {
+                Console.WriteLine("Wrong guess less");
+            }
+            else if (userGuess > 8)
+            {
+                Console.WriteLine("Wrong guess more");
+            }
+
+            else (userGuess == myFavoriteNum)
+                    {
+
+                Console.WriteLine(" You got it");
+            }
+            }
+
+            
+
+            }
         }
     }
-}
+
